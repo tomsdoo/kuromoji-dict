e5ac898caa949afe71e650ef5a7e3d682354d8d1
tomsdoo <toms@live.jp>
2024-11-29 17:15:23 +0900

    npm script build-dict

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vitest/config";
 import { fileURLToPath } from "url";
-import { dirname } from "path";
+import { join, dirname } from "path";
 
 export default defineConfig({
   resolve: {
     alias: {
       "@@": dirname(fileURLToPath(import.meta.url)),
-    }
+      "@": join(dirname(fileURLToPath(import.meta.url)), "src"),
+    },
   },
 });
