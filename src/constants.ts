de439da4a2f59fd87a5425634d6fd5ceb6ff7913
tomsdoo <toms@live.jp>
2025-01-11 08:21:21 +0900

    docker compose

--- src/constants.ts
+++
@@ -1,5 +0,0 @@
-export const APP_NAME = "kuromoji-dict";
-
-export const REPOSITORY_NAME = `${APP_NAME}-build-dict-image`;
-export const TAG_NAME = "v1.0.0";
-export const CONTAINER_NAME = `${APP_NAME}-build-dict-instance`;
